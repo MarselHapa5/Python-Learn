a1d22dc880482bd375f879c265f6fc6df7cd57d7
MarselHapa5Phone <marselhapa@gmail.com>
2022-01-11 11:52:44 +0400

    First commit from phone(termux)

---
+++ shortCalculator.py
@@ -0,0 +1,15 @@
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+print (Back.BLUE)
+
+a = float(input("Первое число: "))
+print (Back.GREEN)
+operator=input("Оператор: ")
+print (Back.BLUE)
+b = float(input("Второе число: "))
+print (Back.RED)
+if operator="+"
+ answer = a + b
+ print(answer) 
+
