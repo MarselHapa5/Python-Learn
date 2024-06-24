b6f0787c79e1efc396ab95813646a91a337461d2
MarselHapa5 <93425197+MarselHapa5@users.noreply.github.com>
2024-06-25 02:27:49 +0400

    mem script inspired by VotV

---
+++ votvConsole.py
@@ -0,0 +1,23 @@
+while True:
+    inputText = input("$ ")
+    if (inputText == "boobs"):
+        print("Try not to do milk")
+        print("(.)(.)")
+
+    if (inputText == "ariral.boobs"):
+        print("Try not to do milk :)))")
+        input("Press 'Enter'")
+        print("(.)(.)")
+        print("(.)(.)")
+        print("(.)(.)")
+
+    if (inputText == "ariral.pls"):
+        print("Suck some huy, u r not in game)")
+
+    if (inputText == "sv.cheats"):
+        print("We'll ban u!")
+        input("DONT PRESS ENTER!!!")
+        print("Server Gospodni")
+
+    if (inputText == "telnet" or "ssh"):
+        print("Соси бибу я не умею юзать requests)")
