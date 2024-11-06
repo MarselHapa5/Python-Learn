f06f228e881fb7bc2960e93fbba7cee4fc410c9d
MarselHapa5 <93425197+MarselHapa5@users.noreply.github.com>
2024-11-07 00:25:19 +0400

    changed a lot of elifs for one dict

--- calculator.py
+++ calculator.py
@@ -1,38 +1,20 @@
-
-from colorama import init
-from colorama import Fore, Back, Style
-init()
-repeat = 1
-while repeat == 1:
-    print (Back.BLUE)
-    exit = input("Вы хотите выйти из программы(если вы хотите выйти напечатайте Yes если не хотите напечатайте No)")
-    if (exit == "Yes"):
-        quit()
-    if (exit == "No"):
-        print("Хорошо")
+while True:
     a = float(input("Первое число: "))
-    print (Back.GREEN)
     operator=input("Оператор: ")
-    print (Back.BLUE)
     b = float(input("Второе число: "))
-    print (Back.RED)
-    if (operator == "+"):
-        answer = a + b
-        print("Ответ:")
-        print(answer)
-    if (operator == "+"):
-        answer = a - b
-        print("Ответ:")
-        print(answer)
-    if (operator == "*"):
-        answer = a * b
-        print("Ответ:")
-        print(answer)
-    if (operator == "/"):
-        answer = a / b
-        print("Ответ:")
-        print(answer)
-    if (operator == "**"):
-        answer = a ** b
-        print("Ответ:")
-        print(answer)
+    operators = {
+        "+":f"Ответ: {a + b}",
+        "-":f"Ответ: {a - b}",
+        "*":f"Ответ: {a * b}",
+        "/":f"Ответ: {a / b}",
+        "**":f"Ответ: {a ** b}",
+        "^":f"Ответ: {a ** b}",
+        "//":f"Ответ: {a // b}",
+        "%":f"Ответ: {a % b}"
+    }
+    
+    if operators.get(operator, None) != None:
+        print(operators.get(operator, "Вы ввели несуществующий оператор"))
+    else: 
+        print()
+    
